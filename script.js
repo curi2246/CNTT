4da837642ff0d304c27f9005e1c8ecc3daed449d
curi2246 <ljk601512090@gmail.com>
2026-01-10 18:13:41 +0900

    Update script.js

--- script.js
+++ script.js
@@ -10,6 +10,8 @@ let charIndex = 0;
 function typeLine() {
   if (lineIndex >= lines.length) return;
 
+  terminal.innerHTML = "";
+
   if (!terminal.children[lineIndex]) {
     const p = document.createElement("p");
     terminal.appendChild(p);
